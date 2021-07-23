830c7afceb94cc4ff7e95015090318789d9acc2a
Gal Sisso <gsplashed@gmail.com>
2021-07-23 15:46:10 +0300

    firebase

--- app/src/main/java/com/example/openugame/MainActivity.java
+++ app/src/main/java/com/example/openugame/MainActivity.java
@@ -3,6 +3,14 @@ package com.example.openugame;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.auth.*;
+import static android.content.ContentValues.TAG;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +18,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        DatabaseReference myRef = FirebaseDatabase.getInstance()
+                .getReference();
+
+        myRef.setValue("test23");
+
+
+
     }
 }
