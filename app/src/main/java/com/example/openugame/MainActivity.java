de1f33064749135abc18e848c7245d572e67de9b
omer.tamir <omer.tamir@shutterfly.com>
2021-07-24 19:11:44 +0300

    update
    Signed-off-by: omer.tamir <omer.tamir@shutterfly.com>

--- app/src/main/java/com/example/openugame/MainActivity.java
+++ app/src/main/java/com/example/openugame/MainActivity.java
@@ -37,86 +37,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         FirebaseApp.initializeApp(this);
         Button b1 = (Button) findViewById(R.id.button);
-
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
-        // Create a new user with a first and last name
-        Map<String, Object> user = new HashMap<>();
-        user.put("first", "Ada");
-        user.put("last", "Lovelace");
-        user.put("born", 1815);
-
-// Add a new document with a generated ID
-        FirebaseAppCheck firebaseAppCheck = FirebaseAppCheck.getInstance();
-        firebaseAppCheck.installAppCheckProviderFactory(
-                SafetyNetAppCheckProviderFactory.getInstance());
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         database.setPersistenceEnabled(false);
         DatabaseReference myRef = database.getReference();
-        DatabaseReference connectedRef = FirebaseDatabase.getInstance().getReference(".info/connected");
-        connectedRef.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                boolean connected = snapshot.getValue(Boolean.class);
-                if (connected) {
-                    Log.d("Omer", "connected");
-                } else {
-                    Log.d("Omer", "not connected");
-                }
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-                Log.w("Omer", "Listener was cancelled");
-            }
-        });
-        // Read from the database
-        Log.d("Omer", "myDB: "+myRef);
-        Log.d("Omer", "myDB2: "+myRef.getDatabase().getApp());
-        myRef.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                // This method is called once with the initial value and again
-                // whenever data at this location is updated.
-                String value = dataSnapshot.getValue(String.class);
-                Log.d("Omer", "Value is: " + value);
-            }
-
-            @Override
-            public void onCancelled(DatabaseError error) {
-                // Failed to read value
-                Log.w("Omer", "Failed to read value.", error.toException());
-            }
-        });
-        Task t1 = myRef.setValue("Hello, World!");
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 a = a + 1;
-                Log.d("Omer", "myDB1: "+t1.isCanceled());
-                Log.d("Omer", "myDB2: "+t1.isComplete());
-                Log.d("Omer", "myDB3: "+t1.isSuccessful());
-                Log.e("Omer","Pl OK ");
                 String a_str = String.valueOf(a);
-                myRef.setValue(a_str).addOnCompleteListener(new OnCompleteListener<Void>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Void> task) {
-                        Log.e("Omer","Test1");
-                    }
-                }).addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Log.e("Omer","Test2");
-                    }
-                }).addOnCanceledListener(new OnCanceledListener() {
-                    @Override
-                    public void onCanceled() {
-                        Log.e("Omer","Test3");
-
-                    }
-                });
+                myRef.setValue(a_str);
             }
         });
-        myRef.push();
-
     }
 }
